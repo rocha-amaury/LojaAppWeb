694b482376f273049daae1299fda90966ef0e5c9
Amaury <amaury.junior@al.infnet.edu.br>
2023-11-19 05:49:00 -0300

    Ajustes Aula 20231103_1

---
+++ LojaAppWeb/Pages/Detalhes.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LojaAppWeb.Pages
+{
+    public class DetalhesModel : PageModel
+    {
+        public void OnGet(int id)
+        {
+
+        }
+    }
+}
