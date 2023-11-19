b71745d725a1b90f1d3620bf31e83774bcc34d07
Amaury <amaury.junior@al.infnet.edu.br>
2023-11-19 05:07:55 -0300

    Ajustes aula 20231101.

---
+++ LojaAppWeb/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace LojaAppWeb.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public string ImagemUri { get; set; }
+    public double Preco { get; set; }
+    public bool EntregaExpressa { get; set; }
+    public DateTime DataCadastro { get; set; }
+
+}
--- LojaAppWeb/Pages/Index.cshtml.cs
+++ LojaAppWeb/Pages/Index.cshtml.cs
@@ -1,11 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using LojaAppWeb.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LojaAppWeb.Pages;
 
 public class IndexModel : PageModel
 {
+
+    public IList<Produto> ListaProdutos { get; set; }
+
     public void OnGet()
     {
+        ViewData["Title"] = "Home page";
 
-    }
+        ListaProdutos = new List<Produto>()
+        {
+            new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Ancho Intermezzo",
+                Descricao = "Ancho Intermezzo",
+                ImagemUri= "/images/01_ancho_intermezzo.webp",
+                Preco =19.00,
+                EntregaExpressa= true,
+                DataCadastro = DateTime.Now
+            },
+            
+            new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Prime Rib Bassi Marfrig",
+                Descricao = "Prime Rib Bassi Marfrig",
+                ImagemUri= "/images/02_prime_rib_bassi_marfrig.webp",
+                Preco =29.00,
+                EntregaExpressa= false,
+                DataCadastro = DateTime.Now
+            },
+            
+            new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Ancho Bassi Marfrig",
+                Descricao = "Ancho Bassi Marfrig",
+                ImagemUri= "/images/01_ancho_intermezzo.webp",
+                Preco =22.00,
+                EntregaExpressa= true,
+                DataCadastro = DateTime.Now
+            },
+            
+            new Produto
+            {
+                ProdutoId = 4,
+                Nome = "T-Bone Intermezzo",
+                Descricao = "T-Bone Intermezzo",
+                ImagemUri= "/images/01_ancho_intermezzo.webp",
+                Preco =25.00,
+                EntregaExpressa= false,
+                DataCadastro = DateTime.Now
+            },
+
+
+            //05 - Shoulder Black VPJ
+            //06 - Flat Iron Intermezzo
+            //07 - Chuck Steak Intermezzo
+            //08 - Prime Rib Bassi Marfrig
+            //09 - Maminha Bassi Marfrig
+            //10 - Tomahawk Cara Preta
+
+        };
+    }    
+
 }
